2c6024dcb4bdf138a164601ffe103a8beffde002
Maria Matejka <mq@ucw.cz>
2022-12-12 20:32:03 +0100

    SmraD: parsing the packets

--- smrad/smrad.py
+++ smrad/smrad.py
@@ -1,3 +1,4 @@
+import datetime
 import socket
 import struct
 
@@ -10,5 +11,15 @@ sock.bind((UDP_IP, UDP_PORT))
 
 while True:
     data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
-    u = struct.unpack("HHBBBBI", data)
+    u = dict(zip([
+        "temp",
+        "hum",
+        "tcrc",
+        "hcrc",
+        "status",
+        "ident",
+        "counter",
+        ],
+        struct.unpack("HHBBBBI", data)))
+    u["now"] = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     print(f"received message: {data} interpreted as {u}")
